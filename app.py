a31a90b709776b4476226a63787744861449c983
siannas <massive.annas@gmail.com>
2018-10-27 15:07:17 +0700

    fixed bug

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-#from scrape import *
-import urllib.request
-from bs4 import BeautifulSoup
+from scrape import *
 
 from flask import Flask, request, abort
 
@@ -44,42 +42,6 @@ line_bot_api = LineBotApi('MNBTHTgxlClDXGlY7TaJSz9ML46xmFXcAT6E2cSpYDyblQfKGA0cq
 handler = WebhookHandler('e0e193b4b0719aa0d83d9695edeffbad')
 #===========[ NOTE SAVER ]=======================
 notes = {}
-
-def cariKerja ():
-    url = 'https://www.jobstreet.co.id/id/job-search/job-vacancy.php?ojs=1'
-    #---------------Make_connection_https--------------#
-    req = urllib.request.Request(url, headers={'User-Agent' : "Magic Browser"})
-    con = urllib.request.urlopen(req)
-    page = BeautifulSoup(con, 'html.parser')
-    
-    #containers = page.body.div.find("div", "container").div
-    containers = page.find(id="job_listing_panel")
-    #print(containers)
-    
-    result = {}
-    idx = 1
-    while idx <= 3:
-        id_tag = "job_ad_"+ str(idx)
-        cntr = containers.find("div",id=id_tag) 
-        
-        gambar = cntr.find("img", "img-company-logo")['data-original']
-        job = cntr.find("h2").text
-        company = cntr.h3.a.span.text
-        lokasi = cntr.ul.li.span.text
-        link = cntr.find("div", "position-title").a['href']
-        
-        result[idx] = {
-            "gambar":gambar,
-            "job":job,
-            "company" :company,
-            "lokasi":lokasi,
-            "link":link
-        }
-        
-        idx += 1
-    #print(result)
-    return result
-
 
 def inputmhs(nrp, nama, kosan):
     r = requests.post("http://www.aditmasih.tk/api_pannas/insert.php", data={'nrp': nrp, 'nama': nama, 'alamat': kosan})
